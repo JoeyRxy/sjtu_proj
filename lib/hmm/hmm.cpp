a2503e46ec73d58d36bf5ee1e524e42554aab184
renxingyu <joeyrxy@gmail.com>
2022-06-07 11:00:11 +0800

    HMM improved

--- lib/hmm/hmm.cpp
+++ lib/hmm/hmm.cpp
@@ -2,14 +2,30 @@
 
 namespace rxy {
 
+static void pM(std::list<LocationPtr> const & loc_set, Markov const & markov, LocationPtr const & loc) {
+    for (auto & prev: loc_set) {
+        auto prob = *markov(prev, loc);
+        if (prob > 0) {
+            std::cout << prev->point << " -> " << loc->point << ": " << prob << '\n';
+        }
+    }
+    std::cout << "done" << std::endl;
+}
+
+static void pLP(std::unordered_map<LocationPtr, Prob> const & _m) {
+    for (auto const & [loc, prob] : _m) {
+        std::cout << loc->point << ": " << prob << std::endl;
+    }
+}
+
 /**
- * @brief Viterbi algorithm for HMM in time sequence { 1, 2, ... T }
+ * @brief Viterbi algorithm for HMM in time sequence { 0, 1, 2, ... T - 1 }
  * Basic idea: dynamic programming. define dp(t, l) as the max probability of being in state(location) l at time t.
  * then, we have the relation formula between (t - 1, l') and (t, l):
- * dp(t, l) = max_{l'} dp(t-1, l') * P(l' -> l) * P(l -> r)
+ * dp(t, l) = max_{l'} dp(t - 1, l') * P(l' -> l) * P(l -> r)
  * where P(l -> r) is the transition probability from location l' to location l, and P(l -> r) is the emission probability of r at state(location) l.
  * @param markovs: a list of markov transition probability functions, corresponding to the time
- * sequence { 1, 2, ... T - 1 } (Attention: the LAST time T is not used). markovs[t - 1] means the
+ * sequence { 0, 1, 2, ... T - 2 } (Attention: the LAST time T - 1 is not used). markovs[t - 1] means the
  * markov transition probability function transited from time t - 1 to time t.
  * @param init_prob: the initial probability of each location at the first time step t = 1.
  * @param emission_probs: the rsrp emission probability P(X|L) corresponding to every location at
@@ -27,13 +43,23 @@ std::vector<LocationPtr> const HMM::operator()(std::vector<MarkovPtr> const& mar
     std::unordered_map<LocationPtr, Prob> prev, cur;
     std::vector<std::unordered_map<LocationPtr, LocationPtr>> psi(T - 1);
     // t = 0
+    bool all_zero = true;
     for (auto&& loc : *loc_set) {
-        cur[loc] = init_prob.at(loc) * emission_probs[0][loc];
+        auto p = init_prob.at(loc) * emission_probs[0].at(loc);
+        if (p != Prob::ZERO) {
+            all_zero = false;
+        }
+        cur[loc] = p;
     }
+    if (all_zero) {
+        throw std::runtime_error("all zero for start");
+    }
+    std::vector<LocationPtr> ret(T);
+    int start = 0;
     for (int t = 1; t < T; ++t) {
         auto const& markov = *markovs[t - 1];
         std::swap(prev, cur);
-        cur.clear();
+        // cur.clear();
         for (auto&& loc : *loc_set) {
             Prob max_prob;
             LocationPtr max_loc = nullptr;
@@ -44,8 +70,39 @@ std::vector<LocationPtr> const HMM::operator()(std::vector<MarkovPtr> const& mar
                     max_loc = prev_loc;
                 }
             }
-            cur[loc] = max_prob * emission_probs[t][loc];
-            psi[t - 1][loc] = max_loc;
+            if (max_loc) {
+                max_prob *= emission_probs[t].at(loc);
+                if (max_prob != Prob::ZERO) {
+                    all_zero = false;
+                }
+                cur[loc] = max_prob;
+                psi[t - 1][loc] = max_loc;
+            } else {
+                max_prob = Prob::ZERO;
+                max_loc = nullptr;
+                for (auto && loc : *loc_set) {
+                    if (prev[loc] > max_prob) {
+                        max_prob = prev[loc];
+                        max_loc = loc;
+                    }
+                }
+                ret[t - 1] = max_loc;
+                for (int _t = t - 2; _t >= start; --_t) {
+                    ret[_t] = psi[_t][ret[_t + 1]];
+                }
+                start = t;
+                for (auto && loc : *loc_set) {
+                    auto p = init_prob.at(loc) * emission_probs[t].at(loc);
+                    if (p != Prob::ZERO) {
+                        all_zero = false;
+                    }
+                    cur[loc] = p;
+                }
+                if (all_zero) {
+                    throw std::runtime_error("all zero for t = " + std::to_string(t));
+                }
+                break;
+            }
         }
     }
     Prob max_prob;
@@ -56,9 +113,8 @@ std::vector<LocationPtr> const HMM::operator()(std::vector<MarkovPtr> const& mar
             max_loc = loc;
         }
     }
-    std::vector<LocationPtr> ret(T);
     ret[T - 1] = max_loc;
-    for (int t = T - 2; t >= 0; --t) {
+    for (int t = T - 2; t >= start; --t) {
         ret[t] = psi[t][ret[t + 1]];
     }
     return ret;
